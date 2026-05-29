ca7b817afbc0da9e91853a444b262a928c60f9d4
Kevin Gallarin <gallarinkevinm@gmail.com>
2026-05-29 23:14:45 +0800

    added default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+	const tile = useState('title', () => 'Nuxt 3 + Tailwind CSS');
+</script>
+<template>
+	<div class="min-h-screen bg-gray-100">
+		<Title>{{ tile }}</Title>
+		<nav class="flex items-center justify-between bg-white p-6 shadow">
+			<div>
+				<NuxtLink to="/">Logo</NuxtLink>
+			</div>
+			<div>
+				<ClientOnly>
+					<ul class="flex space-x-8">
+						<li>
+							<NuxtLink to="/">Home</NuxtLink>
+						</li>
+					</ul>
+				</ClientOnly>
+			</div>
+		</nav>
+
+		<!--    content here-->
+		<slot />
+	</div>
+</template>
+
+<style scoped>
+	@reference '@css/app.css';
+	.router-link-active {
+		@apply font-bold text-blue-500;
+	}
+</style>
