6de5fa89f790885495ad8da9c84300a1cc0b7df7
Kevin Gallarin <gallarinkevinm@gmail.com>
2026-05-30 20:29:38 +0800

    test

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,14 +4,6 @@
 
 <template>
 	<ClientOnly>
-		<div>
-			<div v-if="appStore.loading.global">Loading...</div>
-			<div v-if="appStore.isMobile">Mobile View</div>
-			<div v-else-if="appStore.isDesktop">Desktop View</div>
-			<div v-else>Tablet View</div>
-			alavie
-		</div>
+		<div>alavie sample</div>
 	</ClientOnly>
 </template>
-
-<style scoped></style>
