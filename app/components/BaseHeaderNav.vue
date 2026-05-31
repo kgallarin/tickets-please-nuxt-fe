d1f7d8f054f90f1714d97b7caf0174ce50260a12
Kevin Gallarin <gallarinkevinm@gmail.com>
2026-05-31 21:42:36 +0800

    test qodo

--- app/components/BaseHeaderNav.vue
+++ app/components/BaseHeaderNav.vue
@@ -1,4 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	type NavLink = { label: string; href: string };
+
+	withDefaults(
+		defineProps<{
+			navLinks?: NavLink[];
+		}>(),
+		{
+			navLinks: () => [
+				{
+					label: 'Home',
+					href: '/',
+				},
+				{
+					label: 'Create Tickets',
+					href: '/tickets/create',
+				},
+			],
+		},
+	);
+</script>
 
 <template>
 	<nav class="flex items-center justify-between bg-white p-6 shadow">
@@ -8,8 +28,8 @@
 		<div>
 			<ClientOnly>
 				<ul class="flex space-x-8">
-					<li>
-						<NuxtLink to="/">Home</NuxtLink>
+					<li v-for="link in navLinks" :key="link.label">
+						<NuxtLink :to="link.href"> {{ link.label }}</NuxtLink>
 					</li>
 				</ul>
 			</ClientOnly>
