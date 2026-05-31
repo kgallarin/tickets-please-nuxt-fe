60ff3ce9658b41bbb3903a13ec460f1782b475ad
Kevin Gallarin <gallarinkevinm@gmail.com>
2026-05-31 19:59:11 +0800

    improve pr checks workflow

--- tests/unit/components/base-header-nav.nuxt.spec.ts
+++ tests/unit/components/base-header-nav.nuxt.spec.ts
@@ -7,7 +7,7 @@ import { describe, expect, test } from 'vitest';
 const renderBaseHeaderNav = async (): Promise<RenderResult> => {
 	return renderSuspended(BaseHeaderNav);
 };
-describe('BaseHeaderNav', (): void => {
+describe('BaseHeaderNav', (): null => {
 	test('renders home link', async (): Promise<void> => {
 		const { getByText } = await renderBaseHeaderNav();
 
