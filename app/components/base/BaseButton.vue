7c7b4de78d24f0ef0dcabbd610b9a8ae7ebfa1a1
Kevin Gallarin <gallarinkevinm@gmail.com>
2026-06-05 06:48:46 +0800

    login page + seo + main wrapper for pages/layouts

---
+++ app/components/base/BaseButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+	withDefaults(
+		defineProps<{
+			label?: string;
+			loading?: boolean;
+		}>(),
+		{
+			label: '',
+			loading: false,
+		},
+	);
+</script>
+
+<template>
+	<button
+		class="cursor-pointer rounded-md bg-gray-300 px-4 py-2 text-sm text-white hover:bg-gray-400"
+		:disabled="loading"
+	>
+		{{ loading ? 'loading' : label }}
+	</button>
+</template>
---
+++ app/pages/auth/login.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+	definePageMeta({
+		layout: 'blank',
+	});
+
+	useSeoMeta({
+		title: 'Login',
+		description: 'Login to your account',
+	});
+
+	const title = useState('title', () => 'Login');
+</script>
+
+<template>
+	<div class="flex h-full w-full max-w-md">
+		<Title>{{ title }}</Title>
+
+		<div class="flex w-full flex-col gap-y-2.5">
+			<div class="font-linowrite mb-8 text-center text-xl capitalize">Welcome back!</div>
+
+			<base-input label="Email" type="email" />
+			<base-input label="Password" type="password" />
+
+			<base-button class="mt-8" label="login" />
+		</div>
+	</div>
+</template>
+x
