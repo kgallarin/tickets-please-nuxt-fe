50f22a41eb2ec4f58ad8ca0984f516d8ca9ae2dd
Kevin Gallarin <gallarinkevinm@gmail.com>
2026-06-10 02:32:37 +0800

    authors-list

--- app/pages/authors/index.vue
+++ app/pages/authors/index.vue
@@ -12,7 +12,14 @@
 </script>
 
 <template>
-	<user-row v-for="item in items" :key="item.id" />
+	<div>
+		<div class="font-linowrite mt-4 flex w-full divide-x divide-gray-100 border-b border-gray-200 pb-3">
+			<div class="flex-1 text-center">Name</div>
+			<div class="flex-1 text-center">Email</div>
+			<div class="flex-1 text-center">Admin</div>
+		</div>
+		<user-row v-for="item in items" :key="item.id" :user="item" />
+	</div>
 </template>
 
 <style scoped></style>
